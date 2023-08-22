795fd50059fd53b475e10b55f2c522f1d9c02f41
NataliiaSpas2002 <142512005+NataliiaSpas2002@users.noreply.github.com>
2023-08-23 00:34:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+name = 'Nataliia'
+day = 'August 22'
+print(f'"Good day {name}! {day} is a perfect day to learn some python."')
 
+s = '"Good day {}! {} is a perfect day to learn some python."'
+print(s.format(name, day))
+
+first_name = 'nataliia'
+last_name = 'spasenko'
+print('Hello ' + first_name.title() + ' ' + last_name.title() + '!')
+
+a = 5
+b = 9
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
