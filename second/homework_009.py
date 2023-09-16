19892a6e6872a477247917d2c8f4f430e30d3105
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2023-09-16 19:58:01 +0300

    g

---
+++ second/homework_009.py
@@ -0,0 +1,30 @@
+
+# Topic 8
+# Task 1
+
+def favourite_movie(movie):
+    print('My favourite movie is ' + movie)
+favourite_movie('Inception.')
+
+# Task 2
+def my_function(country, capital):
+    result = {country: capital}
+    return result
+
+print(my_function('Denmark', 'Copenhagen'))
+
+# Task 3
+import math
+def make_operation(operator, *args):
+    if operator == "+":
+        return sum(args)
+    elif operator == "-":
+        return args[0] - sum(args[1:])
+    elif operator == '*':
+        return math.prod(args[:])
+    else:
+        return 'Your number is wrong'
+print(make_operation( '+',3,4))
+print(make_operation('-', 3,4))
+
+print(make_operation('a', 3,4,5))
