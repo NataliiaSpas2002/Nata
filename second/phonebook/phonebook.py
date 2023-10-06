575407e761165175e56e2216e8e0c6d6db811650
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2023-10-06 16:07:40 +0300

    g

--- second/phonebook/phonebook.py
+++ second/phonebook/phonebook.py
@@ -57,6 +57,24 @@ with open(datafile_path, "w") as file:
 
 
 
+# Topic 11
+# Task 1
+
+with open('myfile.txt', 'w') as file:
+    file.write("Hello file world")
+print("File 'myfile.txt' created successfully")
+
+try:
+    with open('myfile.txt', 'r') as file:
+        content = file.read()
+        print(f"File content: {content}")
+except FileNotFoundError:
+    print(f"File 'myfile.txt' not found.")
+except Exception as e:
+    print(f"En error occurred: {e}")
+
+# python .\second\homework_011.py
+# python .\second\myfile.txt
 
 
 
