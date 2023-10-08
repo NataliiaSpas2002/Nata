ee699afad3a36ddebea7eb19bebc746558b92705
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2023-10-08 23:16:01 +0300

    add 14

--- third/homework_009.py
+++ third/homework_009.py
@@ -1,3 +1,5 @@
+# Topic 9
+# Task 1
 import os
 import sys
 print(sys.path)
@@ -19,19 +21,18 @@ except ImportError:
 else:
     print("Worked second time")
 
+
 working_directory = os.path.dirname(os.path.realpath(__file__))
 file_object = open("temp.txt", "r+")
 file_object.close()
 
+# Task 3
 def count_lines(name):
-    lines = 0
-    with open("temp.txt", 'r') as file:
-        for count, line in enumerate(file):
-            pass
-    lines = count + 1
-    return lines
-print("Number of lines:", count_lines(name=1))
-
+    with open("temp.txt", "r") as file:
+        lines = file.readlines()
+    return len(lines)
+print("Number of lines:", count_lines("temp.txt"))
+#
 def count_chars(name):
     try:
         with open("temp.txt", 'r') as file:
@@ -41,19 +42,12 @@ def count_chars(name):
         return -1
 print("Number of characters", count_chars(name=1))
 
-def test(name):
-    line_count = count_lines(name=1)
-    char_count = count_chars(name=1)
-    if line_count != -1 and char_count != -1:
-        print(f"File '{name}' has {line_count} lines and {char_count} characters.")
-    else:
-        print(f"File '{name}' not found.")
+def new_task():
+    name = input("Enter file name: ")
+    print(count_lines(name))
+    print(count_chars(name))
+new_task()
 
-        if len(sys.argv) != 2:
-            print("Usage: python mymod.py <input_file>")
-        else:
-            input_file = sys.argv[1]
 
-test(name="temp")
 
 
