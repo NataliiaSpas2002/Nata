7700464cd3d867823e4b02b603f13fde97d0c30e
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2023-10-13 10:34:50 +0300

    add 14

--- second/homework_013.py
+++ second/homework_013.py
@@ -1,23 +1,51 @@
-def plus(a,b):
-    return a+b
-
-if __name__ == '__main__':
-    print(plus(2,2))
-
 # Topic 13
 # Task 1
-def count_local_var():
-    var1 = 10
-    var2 = "Hello"
+def local_func():
+    var1 = 20
+    var2 = "Anna"
     var3 = [1, 2, 3]
 
-    print(locals())
-    # num_var = locals
-    # return len(num_var)
-    local_variables = locals()
-    a = len(local_variables)
+    local_var = locals()
+    num_var = len(local_var)
 
-# print(f"Number of local variables: {num_var}")
+    print(f"Number of local variables: {num_var}")
 
-# Call the function
-count_local_var()
+local_func()
+
+# Task 2
+def my_func():
+    def inner_func(x, y):
+        return x - y
+    return inner_func
+
+print(my_func()(10, 3))
+
+# Task 3
+def choose_func(nums: list, func1, func2):
+    if all(num > 0 for num in nums):
+        return func1(nums)
+    else:
+        return func2(nums)
+
+def square_nums(nums):
+    return [num ** 2 for num in nums]
+
+
+def remove_negatives(nums):
+    return [num for num in nums if num > 0]
+
+
+nums1 = [1, 2, 3, 4, 5]
+
+nums2 = [1, -2, 3, -4, 5]
+
+result1 = choose_func(nums1, square_nums, remove_negatives)
+result2 = choose_func(nums2, square_nums, remove_negatives)
+
+print(result1)
+print(result2)
+
+assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
+
+assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
+
