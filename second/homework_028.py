eb22b417e4af6925ab28970a4f371602eae924bb
NataliiaSpas2002 <nataliyaspasenko@gmail.com>
2023-11-30 16:49:48 +0200

    hw 28 new

--- second/homework_028.py
+++ second/homework_028.py
@@ -6,14 +6,17 @@ def bidirectional_bubble_sort(arr):
     swapped = True
 
     while swapped:
-        # Forward pass (upward)
         swapped = False
-        for i in range(n -1):
+        for i in range(n - 1):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 swapped = True
 
-        # Bacward pass (downward)
+        if not swapped:
+            break
+        swapped = False
+
+        n -= 1
         for i in range(n - 1, 0, -1):
             if arr[i] < arr[i - 1]:
                 arr[i], arr[i - 1] = arr[i - 1], arr[i]
@@ -22,13 +25,10 @@ def bidirectional_bubble_sort(arr):
     return arr
 
 
-my_list = [101, 64, 34, 25, 12, 22, 11, 90, 10]
+my_list = [64, 34, 25, 12, 22, 11, 90]
 sorted_list = bidirectional_bubble_sort(my_list)
 print("Bubble sorted list:", sorted_list)
 
-# The bidirectional bubble sort має часову складність O(n^2),
-# що робить його не дуже ефективним і зазвичай використовується там,
-# де простота важливіша за ефективність.
 
 # Task 2
 
@@ -37,10 +37,9 @@ def merge_sort(array):
     if len(array) > 1:
         mid = len(array) // 2
 
-        left_half = array[:mid]
-        # left_half = [array[i] for i in range(0, mid)]
-        right_half = array[mid:]
-        #right_half = [array[i] for i in range(mid, len(array))]
+        left_half = [array[i] for i in range(0, mid)]
+
+        right_half = [array[i] for i in range(mid, len(array))]
 
         merge_sort(left_half)
         merge_sort(right_half)
@@ -67,5 +66,6 @@ def merge_sort(array):
             k += 1
 
 
-merge_sort(my_list)
-print("Merge sorted list:", my_list)
+my_second_list = [101, 64, 34, 25, 12, 22, 11, 90, 10]
+merge_sort(my_second_list)
+print("Merge sorted list:", my_second_list)
